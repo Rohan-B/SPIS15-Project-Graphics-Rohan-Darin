948e89953b101ec1f978f93f26253dda1c2027de
Rohan Bhargava <r2bharga@ucsd.edu>
2015-08-20 14:45:12 -0700

    started work on higher res pictures

--- stegdonebeta.py
+++ stegdonebeta.py
@@ -39,8 +39,36 @@ def embedD(toHide,toDo): #embeds degraded picture
     hider.show()
     hider.save('hidden.png')
 
-def embedhighres():
-    pass
+def embedhighres(toHide,toDo):
+     hid = Image.open(toHide)
+    hider = Image.open(toDo)
+
+    width1 = hid.size[0]
+    height1 = hid.size[1]
+
+    width2 = hider.size[0]
+    height2 = hider.size[1]
+    for bigw in range(width2):
+        for bigh in range(height2):
+            (bigR,bigG,bigB)=hider.getpixel((bigw,bigh))
+            bigR = (bigR >> 2)<<2
+            bigG = (bigG >> 2)<<2
+            bigB = (bigB >> 2)<<2
+            hider.putpixel((bigw,bigh),(bigR,bigG,bigB))
+    listofpixels = []
+    for w in range(width1):
+        for h in range(height1):
+            (red,green,blue)=hid.getpixel((w,h))
+            red = red >> 6
+            green = green >> 6
+            blue = blue >> 6
+            (bigR,bigG,bigB)=hider.getpixel((w,h))
+            red = bigR | red
+            green = bigG | green
+            blue = bigB | blue
+            hider.putpixel((w,h),(red,green,blue))
+    hider.show()
+    hider.save('hidden.png')
 
 def debed(uncode):
     hider2 = Image.open(uncode)
