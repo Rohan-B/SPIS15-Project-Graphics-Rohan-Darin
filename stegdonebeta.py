ce2eebb7bffbb4066a0e49334b0735c0168e3c43
Rohan Bhargava <r2bharga@ucsd.edu>
2015-08-25 20:35:42 -0700

    RB/Good Sir Darin worked on fixing the high res steganography

--- stegdonebeta.py
+++ stegdonebeta.py
@@ -64,6 +64,7 @@ def embedhighres(toHide,toDo):
             bigB = (bigB >> 2)<<2
             hider.putpixel((bigw,bigh),(bigR,bigG,bigB))
     colors = []
+    colors2 = []
 
     for w in range(width1):
         for h in range(height1):
@@ -72,29 +73,27 @@ def embedhighres(toHide,toDo):
             red2 = red >> 6
             green2 = green >> 6
             blue2 = blue >> 6
-
             #preserves the third and fourth bits
-            red4 = (red >> 4)&0x0F
-            green4 = (green >> 4)&0x0F
-            blue4 = (blue >> 4)&0x0F
-
+            red4 = (red >> 4)<<2
+            green4 = (green >> 4)<<2
+            blue4 = (blue >> 4)<<2
+            red4 = (red4&0x0F)>>2
+            green4 = (green4&0x0F)>>2
+            blue4 = (blue4&0x0F)>>2
             colors.append([red2,green2,blue2])
-            colors.append([red4,green4,blue4])
-
-    for w in range(0,width1,2):
+            colors2.append([red4,green4,blue4])
+    for w in range(0,width1):
         for h in range(0,height1,2):
             (bigR,bigG,bigB)=hider.getpixel((w,h))
-            red = bigR | colors[w*h/4][0]
-            green = bigG | colors[w*h/4][1]
-            blue = bigB | colors[w*h/4][2]
+            red = bigR | colors[w*h][0]
+            green = bigG | colors[w*h][1]
+            blue = bigB | colors[w*h][2]
             hider.putpixel((w,h),(red,green,blue))
-
-    for w in range(0,width1,2):
-         for h in range(0,height1,2): 
-            red = bigR | colors[w*h+1][0]
-            green = bigG | colors[w*h+1][1]
-            blue = bigB | colors[w*h+1][2]
-            hider.putpixel((w+1,h+1),(red,green,blue))
+            (bigR,bigG,bigB)=hider.getpixel((w,h+1))
+            red = bigR | colors2[w*h][0]
+            green = bigG | colors2[w*h][1]
+            blue = bigB | colors2[w*h][2]
+            hider.putpixel((w,h+1),(red,green,blue))
     hider.show()
     hider.save('hidden2.png')
 
@@ -120,14 +119,14 @@ def debed2(uncode):
     hider2 = Image.open(uncode)
     (width2,height2) = hider2.size
     revealed = Image.new("RGB",(width2,height2),'white')
-
+    colors = []
     for w in range(0,width2,2):
         for h in range(0,height2,2):
             (r,g,b)=hider2.getpixel((w,h))
             r = (r <<6)&0xFF
             g = (g <<6)&0xFF
             b = (b <<6)&0xFF
-
+            #print('r:',bin(r))
             (r2,g2,b2)=hider2.getpixel((w+1,h+1))
             r2 = (r2 <<6)&0xFF
             g2 = (g2 <<6)&0xFF
@@ -136,9 +135,15 @@ def debed2(uncode):
             r2 = r2 >> 2
             g2 = g2 >> 2
             b2 = b2 >> 2
-
+            #print('r2:',bin(r2))
             rf = r | r2
             gf = g | g2
             bf = b | b2
-            revealed.putpixel((w,h),(rf,gf,bf))
+            #print('rf:',bin(rf))
+            colors.append((rf,gf,bf))
+    x = 0
+    for w in range(0,width2/2):
+        for h in range(0,height2/2):
+            revealed.putpixel((w,h),colors[x])
+            x = x+1
     revealed.show()
