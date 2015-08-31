87f411dcdba463ca4c0bffdbb3078921e38002b2
Rohan Bhargava <r2bharga@ucsd.edu>
2015-08-31 11:32:55 -0700

    started work on message embeding

--- embed_message.py
+++ embed_message.py
@@ -1,6 +1,46 @@
 
 #messaging in pictures
 
-def embed_message(string):
-    return bin('h')
+def embed_message(string, picture):
+    hider = Image.open(picture)
+    width1 = hider.size[0]
+    height1 = hider.size[1]
+
+    for bigw in range(width1):
+        for bigh in range(height1):
+            (bigR,bigG,bigB)=hider.getpixel((bigw,bigh))
+            bigR = (bigR >> 2)<<2
+            bigG = (bigG >> 2)<<2
+            bigB = (bigB >> 2)<<2
+            hider.putpixel((bigw,bigh),(bigR,bigG,bigB))
+    nums = []
+    for c in string:
+        try:
+            x = ord(c)
+            nums.append[x]
+        except TypeError:
+            print 'you did something wrong'
+    fnums = []
+    for y in nums:
+        y1 = bin(y)
+        y1 = y1[2:]
+        for b in range(8-len(y1)):
+            y1 = '0' + y1
+            fnums.append(y1)
+    shortnums = []
+    for x in fnums:
+        x1 = x[0:2]
+        x2 = x[2:4]
+        x3 = x[4:6]
+        x4 = x[6:8]
+        shortnums.append[x1]
+        shortnums.append[x2]
+        shortnums.append[x3]
+        shortnums.append[x4]
+
+    for x in range(0,len(shortnums),2)
+                
+    
+    
+    
     
