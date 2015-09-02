c5cd1864edabace509e3cc72603b28a47306eab0
Darin-Moore <darin-moore1996@hotmail.com>
2015-09-02 14:07:40 -0700

    Update and rename hello.py to mainapp.py

--- mainapp.py
+++ mainapp.py
@@ -26,54 +26,111 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
 # Route that will process the file upload
-@app.route('/upload', methods=['POST'])
-def upload():
+@app.route('/uploadboth', methods=['POST'])
+def uploadboth():
     # Get the name of the uploaded file
-    file = request.files['file']
-    result = check_file(file)
+    file1 = request.files['file1']
+    result1 = check_file(file1)
     if result != '':
-        print "result was not blank, result =", result
-        return result
+        print "result was not blank, result =", result1
+        return result1
     else:
         print "result was blank"
         # Make the filename safe, remove unsupported chars
-        filename = secure_filename(file.filename)
+        filename1 = secure_filename(file1.filename)
 
-        fullFilename = (os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        session['file'] = fullFilename
-        print "session['file'] =" ,session['file']
+        fullFilename1 = (os.path.join(app.config['UPLOAD_FOLDER'], filename1))
+        session['file1'] = fullFilename1
+        print "session['file1'] =" ,session['file1']
         # Move the file form the temporal folder to
         # the upload folder we setup
-        file.save(fullFilename)
+        file1.save(fullFilename1)
         # Redirect the user to the uploaded_file route, which
         # will basicaly show on the browser the uploaded file
-        session["filter"]=request.form['filters']
-        newImage = processimage(session["filter"])
-        return render_template('websiteOutput1.html', newImage = fixupfilename(newImage))
+    # Get the name of the uploaded file
+    file2 = request.files['file2']
+    result2 = check_file(file2)
+    if result != '':
+        print "result was not blank, result =", result2
+        return result2
+    else:
+        print "result was blank"
+        # Make the filename safe, remove unsupported chars
+        filename2 = secure_filename(file2.filename)
+
+        fullFilename2 = (os.path.join(app.config['UPLOAD_FOLDER'], filename2))
+        session['file2'] = fullFilename2
+        print "session['file2'] =" ,session['file2']
+        # Move the file form the temporal folder to
+        # the upload folder we setup
+        file2.save(fullFilename2)
+        # Redirect the user to the uploaded_file route, which
+        # will basicaly show on the browser the uploaded file
+    session["encodedimage"] = hideSecretMessage(session['file1'], session['file2'])
+    return render_template('websiteOutput1.html', filename = fixFileName(session['encodedimage']))
+
 
 # This route is expecting a parameter containing the name
 # of a file. Then it will locate that file on the upload
 # directory and show it on the browser, so if the user uploads
 # an image, that image is going to be show after the upload
-
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
-
 def getTempFileName(myPrefix):
-    f = tempfile.NamedTemporaryFile(suffix = ".jpg", prefix = myPrefix, delete=False, dir=app.config['UPLOAD_FOLDER'])
+    f = tempfile.NamedTemporaryFile(suffix = ".bmp", prefix = myPrefix, delete=False, dir=app.config['UPLOAD_FOLDER'])
     f.close()
     return f.name
+    
+def embedhighres(toHide,toDo):
+    #embeds picture, when debeded it becomes a full picture 
+    hid = Image.open(toHide)
+    hider = Image.open(toDo)
+    
+    width1 = hid.size[0]
+    height1 = hid.size[1]
 
-@app.route('/')
-def mainPage():
-    return render_template('websiteHome.html')
+    width2 = hider.size[0]
+    height2 = hider.size[1]
 
-@app.route('/encodemessage')
-def encodeMessagePage():
-    return render_template('websiteEncodeMessage.html')
+    for bigw in range(width2):
+        for bigh in range(height2):
+            #sets last two bits of the picture to be hidden to zero
+            (bigR,bigG,bigB)=hider.getpixel((bigw,bigh))
+            bigR = (bigR >> 2)<<2
+            bigG = (bigG >> 2)<<2
+            bigB = (bigB >> 2)<<2
+            hider.putpixel((bigw,bigh),(bigR,bigG,bigB))
+    colors = []
+    colors2 = []
 
-@app.route('/encodemessagepython')
+    for w in range(width1):
+        for h in range(0,height1):
+            #shifts first two bits to become the last two bits
+            (red,green,blue)=hid.getpixel((w,h))
+            red2 = red >> 6
+            green2 = green >> 6
+            blue2 = blue >> 6
+            #preserves the third and fourth bits
+            red4 = (red >> 4)<<2
+            green4 = (green >> 4)<<2
+            blue4 = (blue >> 4)<<2
+            red4 = (red4&0x0F)>>2
+            green4 = (green4&0x0F)>>2
+            blue4 = (blue4&0x0F)>>2
+            (bigR,bigG,bigB)=hider.getpixel((w,h*2))
+            red = bigR | red2
+            green = bigG | green2
+            blue = bigB | blue2
+            hider.putpixel((w,h*2),(red,green,blue))
+            (bigR,bigG,bigB)=hider.getpixel((w,h*2+1))
+            red = bigR | red4
+            green = bigG | green4
+            blue = bigB | blue4
+            hider.putpixel((w,h*2+1),(red,green,blue))
+            #########
+    hider.save('hidden2.png')
+    name = getTempFileName("encodedimage")
+    hider.save(name)
+    return name
+
 def encodeMessageCode():
     hider = request.files['hider']
     hider = Image.open(picture)
@@ -143,4 +200,11 @@ def decodeMessagePage():
 def recoverPicturePage():
     return render_template('websiteDecodePicture.html')
 
+@app.route('/')
+def mainPage():
+    return render_template('websiteHome.html')
+
+@app.route('/encodemessage')
+def encodeMessagePage():
+    return render_template('websiteEncodeMessage.html')
 
