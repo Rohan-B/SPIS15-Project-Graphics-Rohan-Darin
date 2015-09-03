afc484407e90f0c46044af7d373496560f537990
Darin-Moore <darin-moore1996@hotmail.com>
2015-09-02 20:13:17 -0700

    Update mainapp.py

--- mainapp.py
+++ mainapp.py
@@ -50,7 +50,7 @@ def uploadboth():
         # Redirect the user to the uploaded_file route, which
         # will basicaly show on the browser the uploaded file
     session["encodedimage"] = embedhighres(session['file'], session['file2'])
-     return render_template('websiteOutput1.html', filename = fixFileName(session['encodedimage']))
+    return render_template('websiteOutput1.html', filename = fixFileName(session['encodedimage']))
 
 def fixFileName(badfilename):
     goodfilename = "/" + app.config['UPLOAD_FOLDER'] + os.path.basename(badfilename)
